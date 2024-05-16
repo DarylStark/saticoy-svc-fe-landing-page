3066667f24beae505be3810161d7581dcd39d29e
Daryl Stark <daryl@dstark.nl>
2024-05-16 21:06:31 +0200

    Create basic webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
-  return (
-    <h1>Hallo</h1>
-  )
+    return (
+        <div className='flex flex-col h-screen bg-black'>
+            <header className="h-12"></header>
+            <div className="flex-grow flex items-center justify-center">
+                <h1 className='text-6xl text-orange-400'>Saticoy</h1>
+            </div>
+            <footer className="h-12"></footer>
+        </div>
+    )
 }
 
 export default App
