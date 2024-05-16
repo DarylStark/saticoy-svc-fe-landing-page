5a03c33af58d81872ee29799ee32081a43c05539
Daryl Stark <daryl@dstark.nl>
2024-05-16 21:14:51 +0200

    Add SCSS and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 function App() {
     return (
         <div className='flex flex-col h-screen bg-black'>
-            <header className="h-12"></header>
+            <header className="h-20 flex items-center justify-end pr-4">
+                <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded w-24 opacity-50' disabled>Login</button>
+            </header>
             <div className="flex-grow flex items-center justify-center">
                 <h1 className='text-6xl text-orange-400'>Saticoy</h1>
             </div>
-            <footer className="h-12"></footer>
+            <footer className="h-20"></footer>
         </div>
     )
 }
